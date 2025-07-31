d45b9833c25e3269f5a09d0aa85bc5c7f1556f13
JuanDelgado960809 <juanda1287@gmail.com>
2025-07-30 21:24:47 -0500

    Importando libreria nltk en el Main

---
+++ main.py
@@ -0,0 +1,8 @@
+#importar la libreria nltk
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+#definir la ruta donde se almacenaran los datos descargados de nltk
+nltk.data.path.append('C:/Users/juan/AppData/Roaming/nltk_data')
