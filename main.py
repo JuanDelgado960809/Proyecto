5971f3deb6a642acfd98b93385a3b5f21701bd94
JuanDelgado960809 <juanda1287@gmail.com>
2025-07-31 19:49:01 -0500

    Importando trabajo

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ from nltk.probability import FreqDist
 # Definimos un texto en español que queramos analizar
 
 texto = """
-¿Cómo funciona la IA?
-Las Inteligencias artificiales utilizan algoritmos y modelos matemáticos para procesar grandes cantidades de datos y tomar decisiones basadas en patrones y reglas establecidas a través del aprendizaje automático, que es la capacidad de una máquina para aprender de forma autónoma a partir de datos sin ser programada específicamente para hacerlo. De esta manera la IA puede mejorar su precisión y eficiencia con el tiempo.
-Espero que esta información sobre la IA sea de gran apoyo para su formación y aprendizaje.
-"""
+Mi Nombre es Juan David Delgado Macias actualmente laboro en una empresa distribuidora de productos farmaceuticos,
+mi trabajo consiste en la elaboracion de propuestas para contratos de dispensacion, atencion de incidentes TIC,
+mi pasatiempo favorito esta en viajar, compartir con mi familia y descansar.
+
+Mi compañero"""
 
 # Tokenización: Convertimos el texto en una lista de palabras individuales
 palabras = word_tokenize(texto, language= 'spanish')
